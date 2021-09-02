830ef32d891253d4aef0f8146e1197ce1eecfd43
Ren Gian <laurengian@gmail.com>
2021-09-01 21:00:20 -0600

    first commit to repo

---
+++ client/src/components/Camps.js
@@ -0,0 +1,46 @@
+import React, {useState, useEffect, useContext} from "react"
+import {PCContext} from "../context/ParksCampsProvider.js"
+import axios from "axios"
+
+export default function Campgrounds(props) {
+
+    const { id } = props
+    const [oneCamp, setOneCamp] = useState([])
+    const [savedCamp, setSavedCamp] = useState([])
+    const { getCampground, campground } = useContext(PCContext)
+
+    console.log(campground)
+    
+    useEffect(() => {
+        getCampground()
+    }, [])
+
+    const handleCampSave = () => {
+        
+        const campObj = {
+            id: oneCamp.id,
+            name: oneCamp[0].name,
+            description: oneCamp[0].description,
+            images: oneCamp[0].images.url,
+            addresses: oneCamp[0].addresses,
+        }
+        console.log(campObj)
+        axios.post("/my-travel", campObj)
+            .then(res => setSavedCamp(res.data))
+            .catch(err => console.log(err))
+    }
+
+    return (
+        <div className="camps">
+         <h2>Name: {campground.name}</h2>
+            <p>Description: {campground.description}</p>
+            {/* {props.images[0] != undefined ?
+                <img src={props.images[0].url}/>
+                :
+                <p>No Image available</p>
+            } */}
+            {/* <p>Location: {props.addresses}</p> */}
+            <button onClick={handleCampSave}>Save Location</button>
+        </div>
+    )
+}
