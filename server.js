cfb6e9282c7016ba329cc87c21db6ba35d3f3edc
Ren Gian <laurengian@gmail.com>
2021-09-03 19:29:13 -0600

    final server.js edits

--- server.js
+++ server.js
@@ -8,22 +8,25 @@ const path = require("path")
 
 const port = process.env.PORT || 9000;
 const secret = process.env.SECRET || "plants space fang twizzler"
-const uri = process.env.MONGODB_URI || 'mongodb://localhost/9000/herokuPortfolio'
 
 app.use(express.json())
 app.use(morgan('dev'))
 app.use(express.static(path.join(__dirname, "client", "build")))
 
-mongoose.connect(
-  mongoose.connect(uri,
+  mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/trails-guide",
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false
-    }
-  )
-)
+      // useCreateIndex: true,
+      // useFindAndModify: false
+    })
+    .then(()=>{
+      console.log('con to db established')
+   })
+   .catch(err=>{
+      console.log(`db err: ${err.message}`);
+      process.exit(-1)
+   }) 
 
 app.use('/auth', require('./routes/authRouter.js'))
 app.use('/api', expressjwt({ secret: secret, algorithms: ['HS256'] }))
