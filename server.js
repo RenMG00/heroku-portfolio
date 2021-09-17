7a53270679e8d8718c93fdcf1c99d3929f3e013f
Ren Gian <laurengian@gmail.com>
2021-09-16 21:07:19 -0600

    updated database name in mongoose connection string

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json())
 app.use(morgan('dev'))
 app.use(express.static(path.join(__dirname, "client", "build")))
 
-  mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/trails-guide",
+  mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/parks-guide",
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
